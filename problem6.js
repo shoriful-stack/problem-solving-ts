59640791faadd996a796993e1aa7f16ed4dba1f8
Md. Shoriful Islam <sisazid182713@gmail.com>
2025-06-30 11:05:00 +0600

    problem 7 solved

--- problem6.js
+++ problem6.js
@@ -1,26 +1,26 @@
 "use strict";
 {
     //
-    //Write a TypeScript function that takes a parameter of type unknown and uses a type guard to check whether the parameter is an array of numbers. If it is, calculate the sum of the numbers and log it. If it's not, log an error message.
+    // Write a TypeScript function that takes a parameter of type unknown and uses a type guard to check whether the parameter is an array of numbers. If it is, calculate the sum of the numbers and log it. If it's not, log an error message.
     const checkType = (input) => {
         if (Array.isArray(input)) {
-            const items = input.every(item => typeof item === 'number');
-            if (items) {
+            if (input.every(item => typeof item === 'number')) {
                 let total = 0;
                 for (const item of input) {
                     total += item;
                 }
-                console.log(total);
+                console.log('Total sum', total);
             }
             else {
-                console.log('Array has non-numbers items');
+                console.log("The given array has non numbers");
             }
         }
         else {
             console.log('Please input an array');
         }
     };
-    const result = checkType([1, 2, 3, 4]);
-    console.log(result);
+    checkType([1, 2, 3, 4, 55]);
+    checkType([1, 2, 3, 4, '55']);
+    checkType('Hello');
     //
 }
---
+++ problem7.ts
@@ -0,0 +1,17 @@
+{
+    //
+    // Create a TypeScript function called findFirstOccurrence that accepts an array and a value to search for. Use generics to make the function work with arrays of any data type. Inside the function, find and return the index of the first occurrence of the value in the array. If the value is not found in the array, return -1 to indicate that. Create sample arrays of different data types (e.g., numbers, strings) and call the findFirstOccurrence function for each of them to display the results.
+
+    const findFirstOccurrence = <T> (arr: T[], value: T) => {
+        return arr.indexOf(value);
+    }
+
+    const numArray = [1, 2, 3, 4];
+    const stringArray = ['banana', 'mango', 4, 'Honey'];
+    console.log(findFirstOccurrence(numArray, 3));
+    console.log(findFirstOccurrence(stringArray, 'Honey'));
+    console.log(findFirstOccurrence(numArray, 33));
+    console.log(findFirstOccurrence(stringArray, 'kola'));
+
+    //
+}
